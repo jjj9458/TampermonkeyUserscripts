9d6614e485e24c011964d170023ee8113cf96e67
jjj9458 <haley80208@gmail.com>
2025-05-13 01:56:24 +0800

    Update TSL_toolA.js

--- src/TSL_toolA/TSL_toolA.js
+++ src/TSL_toolA/TSL_toolA.js
@@ -9,14 +9,15 @@
 // @run-at       document-end
 // @grant        none
 // @license      GPL-3.0
-// @updateURL    https://raw.githubusercontent.com/jjj9458/TampermonkeyUserscripts/main/src/TSL_toolA.user.js
-// @downloadURL  https://raw.githubusercontent.com/jjj9458/TampermonkeyUserscripts/main/src/TSL_toolA.user.js
+
 // @homepageURL  https://github.com/jjj9458/TampermonkeyUserscripts
 // @supportURL   https://github.com/jjj9458/TampermonkeyUserscripts/issues
 // ==/UserScript==
 
 /*
  * 註解版
+ * // @updateURL    https://raw.githubusercontent.com/jjj9458/TampermonkeyUserscripts/main/src/TSL_toolA.user.js
+ * // @downloadURL  https://raw.githubusercontent.com/jjj9458/TampermonkeyUserscripts/main/src/TSL_toolA.user.js
  **/
 
 (function(){
